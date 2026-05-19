1ef1938222a0d88c2ea96220fbd12f2ce2338f45
AcoAtmos <utsmanalisanusi@gmail.com>
2026-05-19 14:45:16 +0700

    add controller for update user(edit user)

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,7 +1,11 @@
-const { get_my_profile, get_my_purchases, get_all_purchases } = require("./user.service");
+const { get_my_profile, update_my_profile, get_my_purchases, get_all_purchases } = require("./user.service");
 
 exports.get_my_profile = async (req, res) => {
     await get_my_profile(req, res);
+};
+
+exports.update_my_profile = async (req, res) => {
+    await update_my_profile(req, res);
 };
 
 exports.get_my_purchases = async (req, res) => {
