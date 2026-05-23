4dff4954d4e5b608fb820cb2755b1ab25858f63f
AcoAtmos <utsmanalisanusi@gmail.com>
2026-05-23 23:00:11 +0700

    update ngrok URL in CORS config

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 // Cors
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://entertain-upcountry-padlock.ngrok-free.dev');
+    res.setHeader('Access-Control-Allow-Origin', 'https://yearning-batch-comment.ngrok-free.dev');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
