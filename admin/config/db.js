e8eeea489df79d1d1f107bfdaa45d64a2a036c2f
AcoAtmos <utsmanalisanusi@gmail.com>
2026-06-09 13:13:20 +0700

    refactor: migrate database config to drizzle

--- admin/config/db.js
+++ admin/config/db.js
@@ -1,20 +1,3 @@
-const {Pool} = require('pg');
-const dotenv = require("dotenv");
-dotenv.config();
+const { db } = require('../../db');
 
-let pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-const db = pool;
-pool.on('connect', () => {
-    console.log('Connected to database');
-});
-pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-});
-
-module.exports = { db }
+module.exports = { db };
